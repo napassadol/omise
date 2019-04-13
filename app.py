fc080163cf39758ea176ba62d3c32efa76f67fb4
Napassadol Seangchandao <napassadol.s@gmail.com>
2019-04-13 09:05:57 +0700

    update templates

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request
 import omise
-app = Flask(__name__)
+import mysql.connector
+
+app = Flask(__name__, static_folder='templates/styles')
+
+def connectDB():
+    mydb = mysql.connector.connect(
+        host="178.128.111.50",
+        user="admin2",
+        passwd="192837465",
+        database="payment"
+    )
+    return mydb
 
 def charge(token):
     omise.api_secret = 'skey_test_528pq8jk1ws5hvq6lyu'
@@ -12,15 +23,26 @@ def charge(token):
         card=token
     )
 
-@app.route("/")
-def hello():
-    return render_template('index.html', currency=2222)
+@app.route("/", methods=['GET'])
+def index():
+    token = request.args.get("token")
+
+    mydb = connectDB()
+    mycursor = mydb.cursor()
+    querystr = "SELECT price FROM payment WHERE token = '" + str(token) + "';"
+    mycursor.execute(querystr)
+    myresult = mycursor.fetchall()
+    price = int(myresult[0][0]) * 100
+    print(price)
+    return render_template('index.html', currency=price)
+
 
 @app.route("/checkout", methods=['POST'])
-def test():
+def checkout():
     token = request.form.getlist('omiseToken')[0]
     charge(token)
     return render_template('index.html', currency=0)
 
+
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0', port=8000)
